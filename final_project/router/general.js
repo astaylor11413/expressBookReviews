c630d4731f38bbfb3cfb0237e56493eaab14d829
astaylor11413 <astaylor140@gmail.com>
2025-05-16 03:26:13 -0400

    Added functionality of searching for reviews by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -75,8 +75,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let reviewKeys = Object.keys(books[isbn].reviews);
+  reviewKeys.forEach((key)=>{
+    res.send(books[isbn].reviews[key]);
+  })
+  
 });
 
 module.exports.general = public_users;
