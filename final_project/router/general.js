bebcfff7ae4b33ef8e8e2ed5658aa1857e83d28b
astaylor11413 <astaylor140@gmail.com>
2025-05-17 21:45:41 -0400

    completed conversion of functions fors tasks 1-4 with promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,7 +5,6 @@ const users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-
 public_users.post("/register", (req,res) => {
   let username = req.body.username;
   let password = req.body.password;
@@ -22,35 +21,48 @@ public_users.post("/register", (req,res) => {
   }
 });
 
+
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    let myPromise = new Promise((resolve) => {
+    
+    let myPromise = new Promise((resolve,reject) => {
         setTimeout(() => {
-          resolve("Promise resolved")
+            if(books){
+                res.send(JSON.stringify(books,null,4));
+                resolve("Promise resolved");
+            }else{
+                reject(new Error("Failed to send data"));
+            }
+          
         },2000)});
    myPromise.then((result) => {
-        console.log("Status of call: " + result);
-        res.send(JSON.stringify(books,null,4));
+        console.log("Status of request:"+result);
+    }).catch((error) => {
+        res.send("Error: Coud not read in the books database");
     });
+    
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    //needs to be implemented
     const isbn = req.params.isbn;
     if(isbn){
-        if(books[isbn]){
-            let myPromise = new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve("Promise resolved")
-                },2000)});
-           myPromise.then((result) => {
-                console.log("Status of call: " + result);
-                res.send(books[isbn]);
-            });
-        }else{
-            res.send("Hmm..We could not find that book. Make sure the ISBN number is correct.");
-        }
+        let myPromise = new Promise((resolve,reject) => {
+            setTimeout(() => {
+                if(books[isbn]){
+                    res.send(books[isbn]);
+                    resolve("Promise resolved");
+                }else{
+                    reject(new Error("Error: Could not find book with that isbn"));
+                }
+            },2000)});
+       myPromise.then((result) => {
+            console.log("Status of call: " + result);
+        }).catch((err)=>{
+            res.send("Error: Coud not find requested book");
+        });
     }else{
         res.send("ISBN number needed to complete this search.");
     }
@@ -60,29 +72,29 @@ public_users.get('/isbn/:isbn',function (req, res) {
 public_users.get('/author/:author',function (req, res) {
     let author = req.params.author;
     if(author){
-        let keysArray = Object.keys(books);
-        let filteredBookKeys = keysArray.filter((key)=>books[key].author==author);
-        //check if there was a match detected by filter
-        if(filteredBookKeys.length>0){
-            
-            let myPromise = new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve("Promise resolved")
-                },2000)});
-           myPromise.then((result) => {
-                console.log("Status of call: " + result);
-                filteredBookKeys.forEach((key)=>{
-                    res.send(books[key]);
-                })
-            });
-        
-        }else{
-            res.send("No books were found by that author");
-        }
+        let myPromise = new Promise((resolve,reject) => {
+            setTimeout(() => {
+                let keysArray = Object.keys(books);
+                let filteredBookKeys = keysArray.filter((key)=>books[key].author==author);
+                if(filteredBookKeys.length>0){
+                    let filteredBooks = [];
+                    filteredBookKeys.forEach((key)=>{
+                        filteredBooks.push(books[key]);
+                    })
+                    res.send("Here are the books written by "+author+":"+JSON.stringify(filteredBooks));
+                    resolve("Promise resolved")
+                }else{
+                    reject(new Error("Error:No books were found by that author"));
+                }
+            },2000)});
+       myPromise.then((result) => {
+            console.log("Status of call: " + result);
+        }).catch((error)=>{
+            res.send("Couldn't find books for requested author.");
+        });
     }else{
         res.send("Author names are needed to complete an author search.");
     }
-
 });
     
 
@@ -90,24 +102,27 @@ public_users.get('/author/:author',function (req, res) {
 public_users.get('/title/:title',function (req, res) {
     let title = req.params.title;
     if(title){
-        let keysArray = Object.keys(books);
-        let filteredBookKeys = keysArray.filter((key)=>books[key].title==title);
-        //check if there was a match detected by filter
-        if(filteredBookKeys.length>0){
-            
-            let myPromise = new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve("Promise resolved")
-                },2000)});
-           myPromise.then((result) => {
-                console.log("Status of call: " + result);
-                filteredBookKeys.forEach((key)=>{
-                    res.send(books[key]);
-                });
-            });
-        }else{
-            res.send("No books were found by that title");
-        }
+        let myPromise = new Promise((resolve,reject) => {
+            setTimeout(() => {
+                let keysArray = Object.keys(books);
+                let filteredBookKeys = keysArray.filter((key)=>books[key].title==title);
+                //check if there was a match detected by filter
+                if(filteredBookKeys.length>0){
+                    let filteredBooks = [];
+                    filteredBookKeys.forEach((key)=>{
+                        filteredBooks.push(books[key]);
+                    })
+                    res.send("Here are the books with the title "+title+":"+JSON.stringify(filteredBooks));
+                    resolve("Promise resolved")
+                }else{
+                    reject(new Error("Error:No books were found by that title"));
+                }
+            },2000)});
+        myPromise.then((result) => {
+            console.log("Status of call: " + result);
+        }).catch((error)=>{
+            res.send("Couldn't find books for requested title.");
+        });
     }else{
         res.send("Titles are needed to complete a title search.");
       }
